c9cfd2fc65ca2102080f4aa23740c48199a8b2d8
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2024-04-14 21:32:22 +0200

    added functionality to the button that calls a function that creates a random button then the next one is to use that button to get a random unicode that will display a dice face

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const btnRoll = document.getElementById('btnRoll');
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+btnRoll.onclick = function() {
+    let num = RollTheDice();
+    updateList(num);
+
+}
+
+function RollTheDice () {
+    let randomNumber = getRandomNumber(56, 61);
+    let num = "&#98" + randomNumber + ";" ;
+    alert(num);
+    document.getElementById('dice').innerHTML = num;
+    return num
+    
+}
+function updateList(listitem) {
+    let list = document.getElementById('list');
+
+    let li = document.createElement('li');
+    li.textContent = listitem;
+    list.appendChild(li);
+}
