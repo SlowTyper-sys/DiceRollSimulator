dbf6dd45da4222a2559a93e312038d7761d60ecd
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2024-04-14 22:25:00 +0200

    added the spinning feature to the div along with adding updating the list item's inner html

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ btnRoll.onclick = function() {
 function RollTheDice () {
     let randomNumber = getRandomNumber(56, 61);
     let num = "&#98" + randomNumber + ";" ;
-    alert(num);
+    //alert(num);
     document.getElementById('dice').innerHTML = num;
     return num
     
@@ -22,6 +22,16 @@ function updateList(listitem) {
     let list = document.getElementById('list');
 
     let li = document.createElement('li');
-    li.textContent = listitem;
+   // li.textContent = listitem;
+
+    let span = document.createElement('span');
+    span.innerHTML = "You Rolled: " + listitem;
+
+    li.appendChild(span);
     list.appendChild(li);
 }
+
+document.getElementById("btnRoll").addEventListener("click", function() {
+    let dice =  document.getElementById("dice");
+    dice.classList.toggle("rotated");
+});
